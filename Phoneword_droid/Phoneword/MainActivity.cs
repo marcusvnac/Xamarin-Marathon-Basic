05c78fd8406782de26ad9b9f3458242454c48e64
Marcus Couto <marcusvnac@gmail.com>
2017-02-23 12:10:01 -0300

    Adding a second Activity.

--- Phoneword_droid/Phoneword/MainActivity.cs
+++ Phoneword_droid/Phoneword/MainActivity.cs
@@ -3,12 +3,15 @@ using Android.Widget;
 using Android.OS;
 using System;
 using Android.Content;
+using System.Collections.Generic;
 
 namespace Phoneword
 {
     [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,6 +23,7 @@ namespace Phoneword
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
             Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+            Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
 
             // Disable the "Call" button
             callButton.Enabled = false;
@@ -49,6 +53,10 @@ namespace Phoneword
                 var callDialog = new AlertDialog.Builder(this);
                 callDialog.SetMessage("Call " + translatedNumber + "?");
                 callDialog.SetNeutralButton("Call", delegate {
+                    // add dialed number to list of called numbers.
+                    phoneNumbers.Add(translatedNumber);
+                    // enable the Call History button
+                    callHistoryButton.Enabled = true;
                     // Create intent to dial phone
                     var callIntent = new Intent(Intent.ActionCall);
                     callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
@@ -58,6 +66,13 @@ namespace Phoneword
 
                 // Show the alert dialog to the user and wait for response.
                 callDialog.Show();
+            };
+            
+            callHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(CallHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
             };
 
         }
