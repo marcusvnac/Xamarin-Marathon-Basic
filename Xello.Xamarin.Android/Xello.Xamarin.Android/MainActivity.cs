2a91a3fe91511ee611c4fd8a83e3d2140ec3bbe8
Marcus Couto <marcus.couto@localiza.com>
2017-02-23 11:50:12 -0300

    First App from Xamarin Marathon

---
+++ Xello.Xamarin.Android/Xello.Xamarin.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Xello.Xamarin.Android
+{
+    [Activity(Label = "Xello.Xamarin.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private int clickCounter;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button clickMeButton = FindViewById<Button>(Resource.Id.ClickMeButton);
+
+            clickMeButton.Click += (object sender, EventArgs e) =>
+            {
+                clickCounter++;
+                clickMeButton.Text = clickCounter + " clicks!";
+            };
+        }
+    }
+}
+
